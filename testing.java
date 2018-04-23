b91dd557e69645bb9ec7decc9f581e703d154f0b
stefanos1316 <stefanos1316@gmail.com>
2018-04-23 11:37:45 +0300

    Adding travis to test multiple jdk versions

---
+++ testing.java
@@ -0,0 +1,8 @@
+public class testing {
+
+
+	public static void main(String[] args) {
+		String version = System.getProperty("java.version");
+		System.out.println("The Java version is " + version);
+	}
+}
