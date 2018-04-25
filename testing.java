a8181d6a226d046bb4e12ceefe15e08a5c0c965a
stefanos1316 <stefanos1316@gmail.com>
2018-04-25 18:31:52 +0300

    Adding travis

--- testing.java
+++ testing.java
@@ -1,6 +1,4 @@
 public class testing {
-
-
 	public static void main(String[] args) {
 		String version = System.getProperty("java.version");
 		System.out.println("The Java version is " + version);
